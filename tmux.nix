9cd020468a9d5c4e215f4284ca33b12e4874eb84
Maas Lalani <maaslalani1@gmail.com>
2019-12-10 03:26:09 -0500

    tmux.nix

--- tmux.nix
+++ tmux.nix
@@ -1,32 +1,37 @@
 let
+
   status = {
     bg = "black";
     position = "top";
     justify = "left";
-    right = " #[bg=white]  %H:%M %a ";
+    right = "' #[bg=white]  %H:%M %a '";
   };
+
   window = {
-    status = {
-      format = "#[fg=white] #I #W ";
-      current.format = "#[bg=white,fg=black] #I #W ";
-    };
+    status-format = "#[fg=white] #I #W ";
+    status-current-format = "#[bg=white,fg=black] #I #W ";
   };
-  pane = {
-    border.style = "fg=black,bg=default";
-    active.border.style = "fg=black,bg=default";
+
+  pane = rec {
+    border-style = "fg=black,bg=default";
+    active-border-style = border-style;
   };
+
+  attrsToConfig = setting: config:
+    (f: attrs: map (name: f name attrs.${name})
+    (builtins.attrNames attrs))
+    (name: value: "set -g ${setting}-" + name + " " + value)
+    config;
+
+  statusString = builtins.concatStringsSep "\n"(attrsToConfig "status" status);
+  windowString = builtins.concatStringsSep "\n"(attrsToConfig "window" window);
+  paneString = builtins.concatStringsSep "\n"(attrsToConfig "pane" pane);
+
 in
   ''
-    set -g status-bg ${status.bg}
-    set -g status-position ${status.position}
-    set -g status-justify ${status.justify}
-    set -g status-right '${status.right}'
-
-    setw -g window-status-format '${window.status.format}'
-    setw -g window-status-current-format '${window.status.current.format}'
-
-    set -g pane-border-style '${pane.border.style}'
-    set -g pane-active-border-style '${pane.active.border.style}'
+    ${statusString}
+    ${windowString}
+    ${paneString}
 
     bind \\ split-window -h
     bind - split-window
