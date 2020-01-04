cdb160a66291eec65f560b04c9c448e2f15725c4
Maas Lalani <maaslalani1@gmail.com>
2020-01-04 14:11:47 -0500

    fzf colors

--- home.nix
+++ home.nix
@@ -82,6 +82,7 @@ in
     };
     shellAliases = import ./aliases.nix;
     initExtra = ''
+      setopt autocd autopushd
       ${sourceFile NIX_PATH}
       ${sourceFile DEV_PATH}
     '';
@@ -111,6 +112,10 @@ in
       VISUAL = EDITOR;
       GIT_EDITOR = EDITOR;
       GOPATH = "$HOME";
+      FZF_DEFAULT_OPTS = "
+      --color=fg:-1,bg:-1,hl:#88c0d0,fg+:#b48ead,bg+:#2e3440,hl+:#b48ead,info:#88c0d0
+      --color=prompt:#88c0d0,pointer:#b48ead,marker:#b48ead,spinner:#81a1c1,header:#81a1c1
+      ";
     };
   };
 
