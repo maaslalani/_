c55da630c00101842d9e013016ca358959efe15b
Maas Lalani <maaslalani1@gmail.com>
2020-02-18 11:31:40 -0500

    zsh autosuggestions

--- home.nix
+++ home.nix
@@ -79,6 +79,15 @@
   programs.zsh = import ./zsh.nix // {
     plugins = [
       {
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.6.3";
+          sha256 = "1h8h2mz9wpjpymgl2p7pc146c1jgb3dggpvzwm9ln3in336wl95c";
+        };
+      }
+      {
         name = "zsh-syntax-highlighting";
         src = pkgs.fetchFromGitHub {
           owner = "zsh-users";
