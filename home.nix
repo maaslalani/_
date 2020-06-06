babdf896afc707a8563fd4e185863f3c4beae043
Maas Lalani <maaslalani1@gmail.com>
2020-06-06 16:41:09 -0400

    Start fresh zsh

--- home.nix
+++ home.nix
@@ -17,7 +17,6 @@
     pkgs.spotify-tui
     pkgs.tree
     pkgs.yarn
-    pkgs.zsh
   ];
 
   programs.alacritty.enable = true;
@@ -26,7 +25,6 @@
   programs.git.enable = true;
   programs.home-manager.enable = true;
   programs.neovim.enable = true;
-  programs.starship.enable = true;
   programs.tmux.enable = true;
   programs.z-lua.enable = true;
 
@@ -77,15 +75,6 @@
     ];
   };
 
-  programs.starship = {
-    enableZshIntegration = true;
-    settings = {
-      kubernetes = {
-        disabled = false;
-      };
-    };
-  };
-
   programs.tmux = {
     baseIndex = 1;
     customPaneNavigationAndResize = true;
@@ -98,26 +87,5 @@
     terminal = "xterm-256color";
   };
 
-  programs.zsh = import ./zsh.nix // {
-    plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-autosuggestions";
-          rev = "v0.6.3";
-          sha256 = "1h8h2mz9wpjpymgl2p7pc146c1jgb3dggpvzwm9ln3in336wl95c";
-        };
-      }
-      {
-        name = "zsh-syntax-highlighting";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-syntax-highlighting";
-          rev = "be3882aeb054d01f6667facc31522e82f00b5e94";
-          sha256 = "0w8x5ilpwx90s2s2y56vbzq92ircmrf0l5x8hz4g1nx3qzawv6af";
-        };
-      }
-    ];
-  };
+  programs.zsh = import ./zsh.nix // {};
 }
--- zsh.nix
+++ zsh.nix
@@ -6,47 +6,4 @@ let
 in
   {
     enable = true;
-    enableCompletion = true;
-    enableAutosuggestions = true;
-    history = {
-      path = "~/.zsh_history";
-      size = 1000;
-      save = 1000;
-    };
-    shellAliases = import ./aliases.nix;
-    initExtra = ''
-      setopt autocd autopushd
-
-      bindkey -v
-      bindkey '^P' up-history
-      bindkey '^N' down-history
-      bindkey '^?' backward-delete-char
-      bindkey '^[[Z' reverse-menu-complete
-
-      ${sourceFile NIX_PATH}
-      ${sourceFile DEV_PATH}
-
-      [ -z "$TMUX" ] && tmux new-session -A -s -
-    '';
-
-    sessionVariables = with builtins; rec {
-      EDITOR = "vim";
-      GIT_EDITOR = EDITOR;
-      VISUAL = EDITOR;
-
-      FZF_DEFAULT_OPTS = "
-        --color=hl:6,fg+:5,bg+:0,hl+:5,info:6,prompt:6
-        --color=pointer:5,marker:5,spinner:4,header:4
-      ";
-
-      GO111MODULE = "on";
-      GOPATH = "$HOME/go";
-
-      KEYTIMEOUT = 1;
-
-      KUBECONFIG = builtins.concatStringsSep ":" [
-        "$HOME/.kube/config"
-        "$HOME/.kube/config.shopify.cloudplatform"
-      ];
-    };
   }
