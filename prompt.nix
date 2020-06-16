d381f184ce84810d602679e5e8353fff3e8d7b9e
Maas Lalani <maaslalani1@gmail.com>
2020-06-16 15:37:52 -0400

    parens around git branch

--- prompt.nix
+++ prompt.nix
@@ -15,7 +15,7 @@ in
     precmd = ''
       precmd() {
         if [ $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
-          GIT_BRANCH=$(git branch --show-current)
+          GIT_BRANCH="($(git branch --show-current))"
           GIT_STATUS=$(git status --porcelain | cut -c2 | tr -d ' \n')
         else
           unset GIT_BRANCH
