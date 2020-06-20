f1c91c9072e9f929623af808a5c78063c1e62534
Maas Lalani <maaslalani1@gmail.com>
2020-06-20 14:25:45 -0400

    remove gutter

--- home.nix
+++ home.nix
@@ -43,7 +43,7 @@
     defaultOptions = [
       "--color=${builtins.concatStringsSep "," [
         "bg+:0" "fg+:5" "header:4" "hl+:5" "hl:6" "info:6"
-        "marker:5" "pointer:5" "prompt:6" "spinner:4"
+        "marker:5" "pointer:5" "prompt:6" "spinner:4" "gutter:-1"
       ]}"
     ];
     enableZshIntegration = true;
