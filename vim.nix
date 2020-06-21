7b36ddaad5445f67e7a21ee4270bb61e39edb7ae
Maas Lalani <maaslalani1@gmail.com>
2020-06-20 22:13:02 -0400

    refactor vim.nix

--- vim.nix
+++ vim.nix
@@ -1,100 +1,93 @@
 with builtins;
 let
-  prefixString = prefix: string: prefix + string;
-  combineAttr = name: value: "${name} ${value}";
-  combineAttrs = attrs: attrValues (mapAttrs (combineAttr) attrs);
-  config' = prefix: attrs: join (map (string: prefix + string) (combineAttrs attrs));
-  config = prefix: attrs: config' "${prefix} " attrs;
-  join = concatStringsSep "\n";
+  togglesConfig = a: concatStringsSep "\n" (attrValues ((mapAttrs (n: v: ("set ${if v then "" else "no"}${n}"))) a));
+  settingsConfig = a: concatStringsSep "\n" (attrValues ((mapAttrs (n: v: ("set ${n}=${toString v}"))) a));
+  mapConfig = p: a: concatStringsSep "\n" (attrValues ((mapAttrs (n: v: ("${p}${n} ${v}"))) a));
 
-  optional = boolean: value: if boolean then (value) else "";
-  no = value: optional (isBool value && !value) "no";
-  equalValue = value: optional (!isBool value) "=${toString value}";
-  settingsAttr = name: value: "set ${no value}${name}${equalValue value}";
-
-  settings = {
+  toggles = {
     autoindent = true;
     autoread = true;
     autowrite = true;
-    backspace = "indent,eol,start";
     backup = false;
-    clipboard = "unnamed";
-    cmdheight = 1;
     compatible = false;
-    concealcursor = "\"\"";
     cursorline = true;
-    encoding = "utf-8";
     errorbells = false;
     expandtab = true;
     hidden = true;
     hlsearch = true;
     ignorecase = true;
     incsearch = true;
-    laststatus = 0;
     lazyredraw = true;
     number = true;
-    numberwidth = 1;
-    printfont = "PragmataPro:h12";
     ruler = true;
-    shiftwidth = 2;
     showcmd = true;
     showmode = false;
-    signcolumn = "yes";
     smartcase = true;
-    softtabstop = 2;
     splitbelow = true;
     splitright = true;
     swapfile = false;
-    synmaxcol = 300;
-    t_vb = "";
-    tabstop = 2;
     timeout = false;
-    timeoutlen = 50;
     ttimeout = true;
     ttyfast = true;
-    undodir = "~/.config/nvim/.undo";
     undofile = true;
-    updatetime = 300;
     visualbell = true;
     wb = false;
     wildmenu = true;
-    wildmode = "longest:full,full";
     wrap = false;
     writebackup = false;
   };
 
+  settings = {
+    backspace = "indent,eol,start";
+    clipboard = "unnamed";
+    cmdheight = 1;
+    concealcursor = "\"\"";
+    encoding = "utf-8";
+    laststatus = 0;
+    numberwidth = 1;
+    printfont = "PragmataPro:h12";
+    shiftwidth = 2;
+    signcolumn = "yes";
+    softtabstop = 2;
+    synmaxcol = 300;
+    t_vb = "";
+    tabstop = 2;
+    timeoutlen = 50;
+    undodir = "~/.config/nvim/.undo";
+    updatetime = 300;
+    wildmode = "longest:full,full";
+  };
+
   leaderKey = "<Space>";
 
-  maps = {
-    normal = {
-      Q = ":q<CR>";
-      S = ":%s//g<Left><Left>";
-    };
+  maps.normal = {
+    Q = ":q<CR>";
+    S = ":%s//g<Left><Left>";
+  };
 
-    silent = {
-      gd = "<Plug>(coc-definition)";
-      gi = "<Plug>(coc-implementation)";
-      gr = "<Plug>(coc-references)";
-      gy = "<Plug>(coc-type-definition)";
-    };
+  maps.silent = {
+    gd = "<Plug>(coc-definition)";
+    gi = "<Plug>(coc-implementation)";
+    gr = "<Plug>(coc-references)";
+    gy = "<Plug>(coc-type-definition)";
+  };
 
-    leader = {
-      "" = "<Nop>";
-      a = ":ALEFix<CR>";
-      f = ":FZF<CR>";
-      gb = ":Gblame<CR>";
-      gd = ":Gdiff<CR>";
-      o = ":silent !open <cWORD><CR>";
-      q = ":q<CR>";
-      r = ":Rg<CR>";
-      t = ":tabnew<CR>";
-      w = ":w<CR>";
-    };
+  maps.leader = {
+    "" = "<Nop>";
+    a = ":ALEFix<CR>";
+    f = ":FZF<CR>";
+    gb = ":Gblame<CR>";
+    gd = ":Gdiff<CR>";
+    o = ":silent !open <cWORD><CR>";
+    q = ":q<CR>";
+    r = ":Rg<CR>";
+    t = ":tabnew<CR>";
+    w = ":w<CR>";
+  };
 
-    visual = {
-      S = ":s//g<Left><Left>";
-      s = ":sort<CR>";
-    };
+  maps.visual = {
+    S = ":s//g<Left><Left>";
+    s = ":sort<CR>";
   };
 
   commands = {
@@ -113,22 +106,20 @@ let
   colorscheme = "nord";
 in
 ''
-  ${join (attrValues (mapAttrs (settingsAttr) settings))}
-
-  ${config ":command" commands}
-  ${config "filetype" filetype}
-
-  ${config "nmap" maps.normal}
-  ${config "nnoremap <silent>" maps.silent}
-  ${config "vmap" maps.visual}
-  ${config' "map <silent> ${leaderKey}" maps.leader}
-
   colorscheme ${colorscheme}
 
+  ${settingsConfig settings}
+  ${togglesConfig toggles}
+
+  ${mapConfig ":command " commands}
+  ${mapConfig "filetype " filetype}
+  ${mapConfig "nmap " maps.normal}
+  ${mapConfig "nnoremap <silent> " maps.silent}
+  ${mapConfig "vmap " maps.visual}
+  ${mapConfig "map <silent> ${leaderKey}" maps.leader}
+
   let loaded_netrw = 0
-
   let g:SuperTabDefaultCompletionType = "<c-n>"
-
   let g:ale_sign_error = '*'
   let g:ale_sign_warning = '~'
 ''
