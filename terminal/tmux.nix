25b5d3124f04be4180d4d5fa2ee083b9a0174095
Maas Lalani <maaslalani1@gmail.com>
2020-06-21 12:41:24 -0400

    refactor tmux.nix

--- terminal/tmux.nix
+++ terminal/tmux.nix
@@ -1,33 +1,38 @@
 with builtins;
 let
-  attrsToConfig = attrs: concatStringsSep "\n" (attrsToSettings attrs);
-  attrsToSettings = attrs: attrValues (mapAttrs (attrToSetting) attrs);
-  attrToSetting = name: value: "set -g ${name} ${value}";
+  attrsToConfig = p: a: concatStringsSep "\n" ((a: attrValues (mapAttrs (n: v: "set -g ${p}${n} ${v}") a)) a);
 
   settings = {
     default-terminal = "'xterm-256color'";
-    status-style = "bg=black";
-    status-right-style = "bg=brightblack,fg=white";
-    status-justify = "left";
-    status-right = "'  %H:%M  '";
-    status-left = "''";
-    pane-border-style = "fg=black,bg=default";
-    pane-active-border-style = "fg=black,bg=default";
     mouse = "on";
   };
 
+  pane = {
+    border-style = "fg=black,bg=default";
+    active-border-style = "fg=black,bg=default";
+  };
+
+  status = {
+    style = "bg=black";
+    right-style = "bg=brightblack,fg=white";
+    justify = "left";
+    right = "'  %H:%M  '";
+    left = "''";
+  };
+
   window = {
-    window-status-style = "fg=white,bg=colour0";
-    window-status-current-style = "fg=cyan,bg=brightblack";
-    window-status-current-format = "' #I:#W '";
-    window-status-format = "' #I:#W '";
+    status-style = "fg=white,bg=colour0";
+    status-current-style = "fg=cyan,bg=brightblack";
+    status-current-format = "' #I:#W '";
+    status-format = "' #I:#W '";
   };
 
   message = {
-    message-command-style = "fg=white,bg=colour0";
-    message-style = "fg=cyan,bg=colour0";
-    mode-style = "fg=cyan,bg=colour0";
+    command-style = "fg=white,bg=colour0";
+    style = "fg=cyan,bg=colour0";
   };
+
+  mode.style = "fg=cyan,bg=colour0";
 
   splits = {
     vertical = "|";
@@ -45,9 +50,12 @@ in {
   shortcut = "a";
   terminal = "xterm-256color";
   extraConfig = ''
-    ${attrsToConfig settings}
-    ${attrsToConfig window}
-    ${attrsToConfig message}
+    ${attrsToConfig "" settings}
+    ${attrsToConfig "pane-" pane}
+    ${attrsToConfig "window-" window}
+    ${attrsToConfig "message-" message}
+    ${attrsToConfig "status-" status}
+    ${attrsToConfig "mode-" mode}
 
     bind ${splits.vertical} split-window -h -c "#{pane_current_path}"
     bind ${splits.horiztonal} split-window -c "#{pane_current_path}"
