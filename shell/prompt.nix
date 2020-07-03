7eca971d9e6056c60fcac1c5eb464129f4796f5e
Maas Lalani <maaslalani1@gmail.com>
2020-07-03 18:11:50 -0400

    indents

--- shell/prompt.nix
+++ shell/prompt.nix
@@ -10,18 +10,17 @@ let
   green = color "green";
   magenta = color "magenta";
   red = color "red";
-in
-  {
-    precmd = ''
-      precmd() {
-        if [ $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
-          GIT_BRANCH="($(git branch --show-current))"
-          GIT_STATUS=$(git status --porcelain | cut -c2 | tr -d ' \n')
-        else
-          unset GIT_BRANCH
-          unset GIT_STATUS
-        fi
-      }
-    '';
-    ps1 = "${blue directory} ${magenta gitBranch} ${red gitStatus} \n%(?.${green "❯"}.${red "❯"}) ";
-  }
+in {
+  precmd = ''
+    precmd() {
+      if [ $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
+        GIT_BRANCH="($(git branch --show-current))"
+        GIT_STATUS=$(git status --porcelain | cut -c2 | tr -d ' \n')
+      else
+        unset GIT_BRANCH
+        unset GIT_STATUS
+      fi
+    }
+  '';
+  ps1 = "${blue directory} ${magenta gitBranch} ${red gitStatus} \n%(?.${green "❯"}.${red "❯"}) ";
+}
--- shell/zsh.nix
+++ shell/zsh.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
+with builtins;
 let
   prompt = import ./prompt.nix;
-in
-with builtins; {
+in {
   autocd = true;
   dotDir = ".config/zsh";
   enable = true;
