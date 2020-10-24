b815838e0b26ff067171bd0577bab06aaecb2c15
maaslalani <maaslalani0@gmail.com>
2020-10-24 17:51:03 -0400

    env refactor

--- shell/env.nix
+++ shell/env.nix
@@ -1,42 +1,25 @@
 let
   prompt = import ./prompt.nix;
+  combine = concatStringSep ":";
 in with builtins; rec {
-  EDITOR = "nvim";
-  CLICOLOR = 1;
-  KEYTIMEOUT = 1;
-  KUBECONFIG = concatStringsSep ":" [
-    "$HOME/.kube/config"
-    "$HOME/.kube/config.shopify.cloudplatform"
-  ];
-  MANPAGER = "sh -c 'col -bx | bat -l man -p'";
-  NIX_PATH = concatStringsSep ":" [
-    "$NIX_PATH"
-    "$HOME/.nix-defexpr/channels"
-  ];
-  GOPATH = "$HOME/.config/go";
-  GOBIN = "${GOPATH}/bin";
   CARGO_BIN = "$HOME/.cargo/bin";
-  NIX_BIN = "$HOME/.nix-profile/bin";
-  PATH = concatStringsSep ":" [
-    "/nix/var/nix/gcroots/dev-profiles/user-extra-profile"
-    NIX_BIN
-    CARGO_BIN
-    GOBIN
-    "/usr/local/bin"
-    "/usr/bin"
-    "/bin"
-    "/opt/local/bin"
-    "/opt/dev/bin/user"
-    "/opt/dev/bin"
-    "$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin"
-    "$GOBO/../spec/$ISE_PLATFORM/bin"
-    "$PATH"
-  ];
-  PROMPT = prompt.ps1;
-  PASSWORD_STORE_DIR = "$HOME/.config/pass";
-  TERM = "xterm-256color";
-  MATHMODELS = "/Users/maas/Documents/School/EECS3311/mathmodels";
-  ISE_PLATFORM = "macosx-x86-64";
-  ISE_EIFFEL = "/Applications/MacPorts/Eiffel_19.05";
+  CLICOLOR = 1;
+  EDITOR = "nvim";
+  EIFFEL_BIN = "$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin";
+  GOBIN = "${GOPATH}/bin";
   GOBO = "$ISE_EIFFEL/library/gobo/svn";
+  GOBO_BIN = "$GOBO/../spec/$ISE_PLATFORM/bin";
+  GOPATH = "$HOME/.config/go";
+  ISE_EIFFEL = "/Applications/MacPorts/Eiffel_19.05";
+  ISE_PLATFORM = "macosx-x86-64";
+  KEYTIMEOUT = 1;
+  KUBECONFIG =  combine [ "$HOME/.kube/config" "$HOME/.kube/config.shopify.cloudplatform" ];
+  MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+  MATHMODELS = "/Users/maas/Documents/School/EECS3311/mathmodels";
+  NIX_BIN = "$HOME/.nix-profile/bin";
+  NIX_PATH = combine [ "$NIX_PATH" "$HOME/.nix-defexpr/channels" ];
+  PASSWORD_STORE_DIR = "$HOME/.config/pass";
+  PATH = combine [ CARGO_BIN GOBIN NIX_BIN EIFFEL_BIN GOBO_BIN "$PATH" ];
+  PROMPT = prompt.ps1;
+  TERM = "xterm-256color";
 }
