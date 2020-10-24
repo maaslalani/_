b59cb8d676941ebd36ea726ec4cce230bc66fb4a
maaslalani <maaslalani0@gmail.com>
2020-10-24 11:39:27 -0400

    git vars

--- programs/git.nix
+++ programs/git.nix
@@ -1,15 +1,19 @@
-{
+let
+  user = "maaslalani";
+  name = "Maas Lalani";
+  email = "${user}0@gmail.com";
+in {
   enable = true;
   extraConfig = {
     color.ui = true;
     credential.helper = "osxkeychain";
     diff.algorithm = "patience";
-    github.user = "maaslalani";
+    github.user = user;
     protocol.version = "2";
     pull.rebase = true;
   };
-  userEmail = "maaslalani0@gmail.com";
-  userName = "Maas Lalani";
+  userEmail = email;
+  userName = name;
   aliases = {
     hist = "log --graph --pretty='''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset''' --all";
     cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 git branch -d";
