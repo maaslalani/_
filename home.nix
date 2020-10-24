5f925dbeb2a0bb776756850569c5e0dcf0647600
maaslalani <maaslalani0@gmail.com>
2020-10-24 17:38:03 -0400

    consistent home-manager program

--- home.nix
+++ home.nix
@@ -1,17 +1,14 @@
 { config, pkgs, ... }:
 {
-  programs.home-manager.enable = true;
-
+  home.packages = import ./pkgs.nix { inherit pkgs; };
+  programs.alacritty = import ./terminal/alacritty.nix;
   programs.bat = import ./programs/bat.nix;
   programs.fzf = import ./programs/fzf.nix;
   programs.git = import ./programs/git.nix;
-  programs.taskwarrior = import ./programs/task.nix;
-  programs.z-lua = import ./programs/z.nix;
-
-  programs.alacritty = import ./terminal/alacritty.nix;
+  programs.home-manager = import ./programs/home.nix;
   programs.neovim = import ./editor/vim.nix { inherit pkgs; };
+  programs.taskwarrior = import ./programs/task.nix;
   programs.tmux = import ./terminal/tmux.nix;
+  programs.z-lua = import ./programs/z.nix;
   programs.zsh = import ./shell/zsh.nix { inherit pkgs; };
-
-  home.packages = import ./pkgs.nix { inherit pkgs; };
 }
