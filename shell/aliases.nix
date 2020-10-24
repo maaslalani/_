f55f7d7e80a64762ace134de95af90bd3fd10491
maaslalani <maaslalani0@gmail.com>
2020-10-24 12:32:23 -0400

    scratch alias
    create a temp file, open vim on that file, once you close vim copy contents of file to clipboard and then rm the temp file

--- shell/aliases.nix
+++ shell/aliases.nix
@@ -62,6 +62,9 @@ rec {
   v = "${vi} .";
   vim = "${vi}";
 
+
+  scratch = "FILE=`mktemp /tmp/scratch.XXXXXX`; vim $FILE +startinsert && pbcopy < $FILE; rm $FILE";
+
   weather = "curl http://v2.wttr.in";
   wiki = "vim ~/wiki/index.wiki";
 }
