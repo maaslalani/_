74317da36e13fa217425511b30588f7c2647c8be
maaslalani <maaslalani0@gmail.com>
2020-10-25 18:36:37 -0400

    condense lua code

--- editor/config.lua
+++ editor/config.lua
@@ -4,20 +4,11 @@ function on_attach(client)
 end
 
 function goimports()
-  local context = { source = { organizeImports = true } }
-  vim.validate { context = { context, "t", true } }
-
   local params = vim.lsp.util.make_range_params()
-  params.context = context
-
+  params.context = { source = { organizeImports = true } }
   local resp = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
-  if resp and resp[1] then
-    local result = resp[1].result
-    if result and result[1] then
-      local edit = result[1].edit
-      vim.lsp.util.apply_workspace_edit(edit)
-    end
+  if resp and resp[1] and resp[1].result and resp[1].result[1] then
+    vim.lsp.util.apply_workspace_edit(resp[1].result[1].edit)
   end
-
   vim.lsp.buf.formatting()
 end
