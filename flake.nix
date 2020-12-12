75f37e0f8357b489a03e44463dcf60d1df3ccff7
maaslalani <maaslalani0@gmail.com>
2020-12-11 21:57:59 -0500

    clean up env and flakes.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Example home-manager from non-nixos system";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.neovim-nightly-overlay.url = "github:mjlbach/neovim-nightly-overlay";
+
+  inputs.home-manager = {
+    url = "github:rycee/home-manager";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, ... }@inputs: {
+    homeConfigurations = {
+      home = inputs.home-manager.lib.homeManagerConfiguration {
+        configuration = { pkgs, ... }: {
+          nixpkgs.overlays = [
+            inputs.neovim-nightly-overlay.overlay
+          ];
+          imports = [];
+        };
+        system = "x86_64-darwin";
+        homeDirectory = "/Users/maas";
+        username = "maas";
+      };
+    };
+    home = self.homeConfigurations.home.activationPackage;
+  };
+}
--- shell/env.nix
+++ shell/env.nix
@@ -1,23 +1,19 @@
-rec {
+let
+  pathJoin = builtins.concatStringsSep ":";
+in rec {
   CARGO_BIN = "$HOME/.cargo/bin";
   CLICOLOR = 1;
   COLORTERM = "truecolor";
   EDITOR = "nvim";
-  EIFFEL_BIN = "$ISE_EIFFEL/studio/spec/$ISE_PLATFORM/bin";
   GOBIN = "${GOPATH}/bin";
-  GOBO = "$ISE_EIFFEL/library/gobo/svn";
-  GOBO_BIN = "$GOBO/../spec/$ISE_PLATFORM/bin";
   GOPATH = "$HOME/.config/go";
-  ISE_EIFFEL = "/Applications/MacPorts/Eiffel_19.05";
-  ISE_PLATFORM = "macosx-x86-64";
   KEYTIMEOUT = 1;
-  KUBECONFIG =  builtins.concatStringsSep ":" [ "$HOME/.kube/config" "$HOME/.kube/config.shopify.cloudplatform" ];
+  KUBECONFIG =  pathJoin [ "$HOME/.kube/config" "$HOME/.kube/config.shopify.cloudplatform" ];
   MANPAGER = "sh -c 'col -bx | bat -l man -p'";
-  MATHMODELS = "$HOME/Documents/School/EECS3311/mathmodels";
   NIX_BIN = "$HOME/.nix-profile/bin";
-  NIX_PATH = builtins.concatStringsSep ":" [ "$NIX_PATH" "$HOME/.nix-defexpr/channels" ];
+  NIX_PATH = pathJoin [ "$NIX_PATH" "$HOME/.nix-defexpr/channels" ];
   PASSWORD_STORE_DIR = "$HOME/.config/pass";
-  PATH = builtins.concatStringsSep ":" [ CARGO_BIN EIFFEL_BIN GOBIN GOBO_BIN NIX_BIN "$PATH" ];
+  PATH = pathJoin [ CARGO_BIN GOBIN NIX_BIN "$PATH" ];
   PROMPT = (import ./prompt.nix).ps1;
   TERM = "xterm-256color";
 }
