f07e1d4a4d4cfc2eec6d50aa2853504248f3f752
maaslalani <maaslalani0@gmail.com>
2020-12-23 14:24:07 -0500

    change map to be under one expression

--- modules/vim.nix
+++ modules/vim.nix
@@ -1,14 +1,16 @@
 { config, pkgs, lib, ... }:
-with builtins; let
-  config = f: a: concatStringsSep "\n" (attrValues (mapAttrs f a));
-  configArray = f: a: concatStringsSep "\n" (map f a);
+let
+  config = f: a: builtins.concatStringsSep "\n" (builtins.attrValues (builtins.mapAttrs f a));
+  configArray = f: a: builtins.concatStringsSep "\n" (builtins.map f a);
 
   autocmdConfig = config (n: v: ("autocmd ${n} ${v}"));
   mapConfig = p: config (n: v: ("${p}${n} ${v}"));
-  settingsConfig = config (n: v: ("set ${n}=${toString v}"));
+  settingsConfig = config (n: v: ("set ${n}=${builtins.toString v}"));
   togglesConfig = config (n: v: ("set ${if v then "" else "no"}${n}"));
-  variablesConfig = config (n: v: ("let ${n}=${toString v}"));
+  variablesConfig = config (n: v: ("let ${n}=${builtins.toString v}"));
   lspConfig = configArray (s: "require'nvim_lsp'.${s}.setup { on_attach = require'completion'.on_attach }");
+
+  leaderKey = "<Space>";
 
   toggles = {
     autoindent = true;
@@ -68,58 +70,56 @@ with builtins; let
     wildmode = "longest:full,full";
   };
 
-  leaderKey = "<Space>";
-  navigationKey = "<C-a>";
-  terminalEscapeKey = "<C-\\><C-n>";
+  maps = {
+    normal = {
+      "<BS>" = "<Plug>(dirvish_up)";
+      Q = ":q<CR>";
+      S = ":%s//g<Left><Left>";
+      "<C-j>" = "<C-w>j";
+      "<C-k>" = "<C-w>k";
+      "<C-h>" = "<C-w>h";
+      "<C-l>" = "<C-w>l";
+    };
 
-  maps.normal = {
-    "<BS>" = "<Plug>(dirvish_up)";
-    Q = ":q<CR>";
-    S = ":%s//g<Left><Left>";
-    "<C-j>" = "<C-w>j";
-    "<C-k>" = "<C-w>k";
-    "<C-h>" = "<C-w>h";
-    "<C-l>" = "<C-w>l";
-  };
+    silent = {
+      gh = "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>";
+      K = "<cmd>lua vim.lsp.buf.hover()<CR>";
+      gI = "<cmd>lua vim.lsp.buf.implementation()<CR>";
+      gr = "<cmd>lua vim.lsp.buf.references()<CR>";
+      gd = "<cmd>lua vim.lsp.buf.definition()<CR>";
+    };
 
-  maps.silent = {
-    gh = "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>";
-    K = "<cmd>lua vim.lsp.buf.hover()<CR>";
-    gI = "<cmd>lua vim.lsp.buf.implementation()<CR>";
-    gr = "<cmd>lua vim.lsp.buf.references()<CR>";
-    gd = "<cmd>lua vim.lsp.buf.definition()<CR>";
-  };
+    leader = {
+      "" = "<Nop>";
+      "=" = "<cmd>lua vim.lsp.buf.formatting()<CR>";
+      N = "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>";
+      W = ":w!<CR>";
+      a = "<cmd>lua vim.lsp.buf.code_action()<CR>";
+      e = ":Dirvish<CR>";
+      f = "<cmd>Telescope fd<cr>";
+      l = "<cmd>lua vim.lsp.buf.formatting()<CR>";
+      n = "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>";
+      o = ":silent !open <cWORD><CR>";
+      p = "\"*p";
+      q = ":q<CR>";
+      r = "<cmd>Telescope live_grep<cr>";
+      sl = ":luafile %<CR>";
+      so = ":sort<CR>";
+      t = ":tabnew<CR>";
+      w = ":w<CR>";
+      y = "\"*y";
+    };
 
-  maps.leader = {
-    "" = "<Nop>";
-    "=" = "<cmd>lua vim.lsp.buf.formatting()<CR>";
-    N = "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>";
-    W = ":w!<CR>";
-    a = "<cmd>lua vim.lsp.buf.code_action()<CR>";
-    e = ":Dirvish<CR>";
-    f = "<cmd>Telescope fd<cr>";
-    l = "<cmd>lua vim.lsp.buf.formatting()<CR>";
-    n = "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>";
-    o = ":silent !open <cWORD><CR>";
-    p = "\"*p";
-    q = ":q<CR>";
-    r = "<cmd>Telescope live_grep<cr>";
-    sl = ":luafile %<CR>";
-    so = ":sort<CR>";
-    t = ":tabnew<CR>";
-    w = ":w<CR>";
-    y = "\"*y";
-  };
+    visual = {
+      "<" = "<gv";
+      ">" = ">gv";
+      s = ":s//g<Left><Left>";
+    };
 
-  maps.visual = {
-    "<" = "<gv";
-    ">" = ">gv";
-    s = ":s//g<Left><Left>";
-  };
-
-  maps.insert = {
-    "<expr> <S-Tab>" = "pumvisible() ? \"\\<C-p>\" : \"\\<S-Tab>\"";
-    "<expr> <Tab>" = "pumvisible() ? \"\\<C-n>\" : \"\\<Tab>\"";
+    insert = {
+      "<expr> <S-Tab>" = "pumvisible() ? \"\\<C-p>\" : \"\\<S-Tab>\"";
+      "<expr> <Tab>" = "pumvisible() ? \"\\<C-n>\" : \"\\<Tab>\"";
+    };
   };
 
   commands = {
