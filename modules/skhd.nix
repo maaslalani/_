48432b3cf82b7ba120061c8456d70e78b5a11dda
maaslalani <maaslalani0@gmail.com>
2021-01-16 16:14:37 -0500

    nixify config files for yabai & skhd

--- modules/skhd.nix
+++ modules/skhd.nix
@@ -1,6 +1,11 @@
 { config, pkgs, libs, ... }:
+with builtins; let
+  config = f: a: concatStringsSep "\n" (attrValues (mapAttrs f a));
+  attrsToConfig = config (n: v: ("${n} : ${v}"));
+  settings = {
+    "hyper - return" = "open /Applications/Alacritty.app";
+  };
+in
 {
-  home.file.".config/skhd/skhdrc".text = ''
-    hyper - return : open /Applications/Alacritty.app
-  '';
+  home.file.".config/skhd/skhdrc".text = attrsToConfig settings;
 }
