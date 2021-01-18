83c7a8d52442abff6ab0d0a2ca22c501551298c8
Maas Lalani <maaslalani0@gmail.com>
2021-01-18 10:36:41 -0500

    application shortcuts (replace thor)

--- modules/skhd.nix
+++ modules/skhd.nix
@@ -3,7 +3,10 @@ with builtins; let
   config = f: a: concatStringsSep "\n" (attrValues (mapAttrs f a));
   attrsToConfig = config (n: v: ("${n} : ${v}"));
   settings = {
-    "hyper - return" = "open /Applications/Alacritty.app";
+    "hyper - a" = "open /Applications/Alacritty.app";
+    "hyper - b" = "open /Applications/Brave\\ Browser.app";
+    "hyper - c" = "open /System/Applications/Calendar.app";
+    "hyper - s" = "open /Applications/Slack.app";
   };
 in
 {
