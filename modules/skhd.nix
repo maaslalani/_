f3a1542d2f0228266eeb0c887fff953086ce8f96
Maas Lalani <maaslalani0@gmail.com>
2021-03-08 17:23:15 -0500

    skhd mappings

--- modules/skhd.nix
+++ modules/skhd.nix
@@ -7,6 +7,12 @@ with builtins; let
     "hyper - b" = "open /Applications/Brave\\ Browser.app";
     "hyper - c" = "open /System/Applications/Calendar.app";
     "hyper - s" = "open /Applications/Slack.app";
+
+    "alt - h" = "yabai -m window --focus west";
+    "alt - j" = "yabai -m window --focus south";
+    "alt - k" = "yabai -m window --focus north";
+    "alt - l" = "yabai -m window --focus east";
+    "alt - f" = "yabai -m window --toggle zoom-fullscreen";
   };
 in
 {
