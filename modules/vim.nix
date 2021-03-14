e9b98e616a6805c09cc0540a377f284c9d7381a5
Maas Lalani <maaslalani0@gmail.com>
2021-03-13 19:30:48 -0500

    goimports

--- modules/vim.nix
+++ modules/vim.nix
@@ -177,7 +177,7 @@ let
     "BufEnter *.nix" = "set ft=nix";
     "BufEnter *.lock" = "set ft=json";
     "BufEnter *.graphql" = "set ft=graphql";
-    "BufWrite *.go" = "lua vim.lsp.buf.formatting()";
+    "BufWrite *.go" = "lua goimports(1000)";
     "CmdLineEnter :" = "set nosmartcase";
     "CmdLineLeave :" = "set smartcase";
     "TermOpen *" = "setlocal nonumber signcolumn=no";
@@ -239,6 +239,27 @@ in
       require'nordbuddy'.use{}
       ${lspSetup nvim.lsp}
       ${treesitterSetup nvim.treesitter}
+      function goimports(timeoutms)
+        local context = { source = { organizeImports = true } }
+        vim.validate { context = { context, "t", true } }
+        local params = vim.lsp.util.make_range_params()
+        params.context = context
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+        if not result or next(result) == nil then return end
+        local actions = result[1].result
+        if not actions then return end
+        local action = actions[1]
+        if action.edit or type(action.command) == "table" then
+          if action.edit then
+            vim.lsp.util.apply_workspace_edit(action.edit)
+          end
+          if type(action.command) == "table" then
+            vim.lsp.buf.execute_command(action.command)
+          end
+        else
+          vim.lsp.buf.execute_command(action)
+        end
+      end
       EOF
     '';
     vimAlias = true;
