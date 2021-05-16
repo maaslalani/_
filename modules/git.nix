a696952e6c0e875e89f74c53ff3893a6590b67a7
Maas Lalani <maaslalani0@gmail.com>
2021-05-16 13:42:15 -0400

    fancy diffs

--- modules/git.nix
+++ modules/git.nix
@@ -15,13 +15,30 @@ in
       protocol.version = "2";
       pull.rebase = true;
     };
+    delta = {
+      enable = true;
+      options = {
+        syntax-theme = "Nord";
+        plus-style = "syntax #132E1F";
+        minus-style = "syntax #36191C";
+        minus-emph-style = "syntax #882728";
+        plus-emph-style = "syntax #226d33";
+        zero-style = "syntax #2E3440";
+        line-numbers = true;
+        line-numbers-minus-style = "#CB443F #1D1D2B";
+        line-numbers-zero-style = "#4C566A";
+        line-numbers-plus-style = "#3EB64F #1D1D2B";
+        line-numbers-left-format = "{nm:^4}";
+        line-numbers-right-format = "{np:^4}";
+      };
+    };
     userEmail = email;
     userName = name;
     aliases = {
       cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 git branch -d";
       hist = "log --graph --pretty='''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset''' --all";
       lo = "log --oneline -n 10";
-      open = "!git config --get remote.origin.url | xargs open";
+      open = "!git config --get remote..url | xargs open | echo";
       undo = "reset HEAD~1 --mixed";
     };
   };
