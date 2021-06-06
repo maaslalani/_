e415afd51c543a1b8fc22c7808ba6b1838432db5
Maas Lalani <maaslalani0@gmail.com>
2021-06-06 13:35:31 -0400

    fix git open

--- modules/git.nix
+++ modules/git.nix
@@ -38,7 +38,7 @@ in
       cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 git branch -d";
       hist = "log --graph --pretty='''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset''' --all";
       lo = "log --oneline -n 10";
-      open = "!git config --get remote..url | xargs open | echo";
+      open = "!git config --get remote.origin.url | xargs open";
       undo = "reset HEAD~1 --mixed";
     };
   };
