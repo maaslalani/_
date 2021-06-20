d20d8ebd092b3fa0b64ff835eea275c26c35f77c
Maas Lalani <maaslalani0@gmail.com>
2021-06-20 11:59:21 -0400

    use config.xdg.configHome variable

--- modules/pass.nix
+++ modules/pass.nix
@@ -3,7 +3,7 @@
   programs.password-store = {
     enable = true;
     settings = {
-      PASSWORD_STORE_DIR = "$HOME/.config/pass";
+      PASSWORD_STORE_DIR = "${config.xdg.configHome}/pass";
       PASSWORD_STORE_CLIP_TIME = "60";
     };
   };
