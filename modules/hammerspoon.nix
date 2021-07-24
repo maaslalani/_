6058ad6bcedb3f6a28e979a2ae06427230d3be8a
Maas Lalani <maaslalani0@gmail.com>
2021-07-24 19:37:02 -0400

    specify sha256

--- modules/hammerspoon.nix
+++ modules/hammerspoon.nix
@@ -2,6 +2,7 @@
 {
   home.file.".hammerspoon/Spoons/SpoonInstall.spoon/init.lua".source = builtins.fetchurl {
     url = "https://raw.githubusercontent.com/Hammerspoon/Spoons/master/Source/SpoonInstall.spoon/init.lua";
+    sha256 = "1r3ka83yxmi65zg0x4x7v2hr96dg0y0p0si7q1grrwc2ygx38kvj";
   };
   home.file.".hammerspoon/init.lua".source = ../lua/hammerspoon.lua;
 }
