f53e3785397ebee5c9379e03177c02ef283624b0
Maas Lalani <maaslalani0@gmail.com>
2021-07-24 22:58:21 -0400

    launch applications

--- lua/hammerspoon.lua
+++ lua/hammerspoon.lua
@@ -1,15 +1,27 @@
 hs.hotkey.bind({'cmd', 'ctrl'}, 'R', hs.reload)
 
+hs.alert.defaultStyle.fillColor = { white = 0, alpha = 0.80 }
+hs.alert.defaultStyle.padding = 22
 hs.alert.defaultStyle.radius = 2
-hs.alert.defaultStyle.fillColor = { white = 0, alpha = 0.75 }
 hs.alert.defaultStyle.strokeColor = { white = 1, alpha = 0.25 }
 hs.alert.defaultStyle.strokeWidth = 5
 hs.alert.defaultStyle.textColor = { white = 1, alpha = 0.8 }
-hs.alert.defaultStyle.textSize = 18
 hs.alert.defaultStyle.textFont = "Menlo"
-hs.alert.defaultStyle.padding = 18
+hs.alert.defaultStyle.textSize = 18
 
 hs.alert.show('Hammerspoon Loaded', 1)
+
+hs.grid.ui.cellColor = {0,0,0,0.25}
+hs.grid.ui.cellStrokeColor = {0.2,0.2,0.2}
+hs.grid.ui.cellStrokeWidth = 5
+hs.grid.ui.cyclingHighlightColor = {0.2,0.2,0.2,0.5}
+hs.grid.ui.cyclingHighlightStrokeColor = {0.2,0.2,0.2,1}
+hs.grid.ui.highlightColor = {0.2,0.2,0.2,0.5}
+hs.grid.ui.highlightStrokeColor = {0.8,0.8,0.8,1}
+hs.grid.ui.highlightStrokeWidth = 10
+hs.grid.ui.selectedColor = {0.6,0.6,0.6,0.2}
+hs.grid.ui.textColor = {1,1,1}
+hs.grid.ui.textSize = 150
 
 local menu = {
   a = {
@@ -17,30 +29,35 @@ local menu = {
     a = {
       name = 'Alacritty',
       action = function()
+        hs.application.launchOrFocus('Alacritty')
         hs.alert('Open Alacritty')
       end
     },
     b = {
-      name = 'Brave Browser',
+      name = 'Brave',
       action = function()
+        hs.application.launchOrFocus('Brave Browser')
         hs.alert('Open Brave Browser')
       end
     },
     c = {
       name = 'Calendar',
       action = function()
+        hs.application.launchOrFocus('Calendar')
         hs.alert('Open Calendar')
       end
     },
     p = {
       name = '1Password',
       action = function()
+        hs.application.launchOrFocus('1Password 7')
         hs.alert('Open 1Password')
       end
     },
     s = {
       name = 'Slack',
       action = function()
+        hs.application.launchOrFocus('Slack')
         hs.alert('Open Slack')
       end
     },
@@ -48,31 +65,35 @@ local menu = {
   g = {
     name = 'Grid',
     action = function()
-      hs.alert('Open Grid')
+      hs.grid.show()
     end
   },
   w = {
     name = 'Window',
     h = {
       name = 'Left',
+      repeatable = true,
       action = function()
         hs.alert('H')
       end
     },
     j = {
       name = 'Down',
+      repeatable = true,
       action = function()
         hs.alert('J')
       end
     },
     k = {
       name = 'Up',
+      repeatable = true,
       action = function()
         hs.alert('K')
       end
     },
     l = {
       name = 'Right',
+      repeatable = true,
       action = function()
         hs.alert('L')
       end
@@ -100,7 +121,12 @@ function setup(modal, menu)
           submenu:enter()
         end
       end
-      modal:bind({}, k, function() modal:exit() action() end)
+      modal:bind({}, k, function()
+        if v.repeatable ~= true then
+          modal:exit()
+        end
+        action()
+      end)
     end
   end
   function modal:entered()
