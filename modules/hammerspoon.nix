65a8f4a587a80e122675883e1b725243b6e9f028
Maas Lalani <maaslalani0@gmail.com>
2021-10-24 14:47:18 -0400

    xdg path for hammerspoon
    defaults write org.hammerspoon.Hammerspoon MJConfigFile '~/.config/hammerspoon/init.lua'

--- modules/hammerspoon.nix
+++ modules/hammerspoon.nix
@@ -1,8 +1,11 @@
 { config, pkgs, libs, ... }:
+let
+  hammerspoonPath = "${config.xdg.configHome}/hammerspoon";
+in
 {
-  home.file.".hammerspoon/Spoons/SpoonInstall.spoon/init.lua".source = builtins.fetchurl {
+  home.file."${hammerspoonPath}/Spoons/SpoonInstall.spoon/init.lua".source = builtins.fetchurl {
     url = "https://raw.githubusercontent.com/Hammerspoon/Spoons/master/Source/SpoonInstall.spoon/init.lua";
     sha256 = "1r3ka83yxmi65zg0x4x7v2hr96dg0y0p0si7q1grrwc2ygx38kvj";
   };
-  home.file.".hammerspoon/init.lua".source = "${pkgs.fnl}/hammerspoon.lua";
+  home.file."${hammerspoonPath}/init.lua".source = "${pkgs.fnl}/hammerspoon.lua";
 }
