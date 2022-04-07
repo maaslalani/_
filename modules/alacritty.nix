dbfe90ef16cfbb040a843bfcdf290eca3cd7ed18
Maas Lalani <maas@lalani.dev>
2022-04-06 23:44:55 -0400

    colors

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -33,26 +33,32 @@
       };
 
       colors = {
-        bright.black = "0x4C566A";
-        bright.blue = "0x81A1C1";
-        bright.cyan = "0x8FBCBB";
-        bright.green = "0xA3BE8C";
-        bright.magenta = "0xB48EAD";
-        bright.red = "0xBF616A";
-        bright.white = "0xECEFF4";
-        bright.yellow = "0xEBCB8B";
+        bright = {
+          black = "0x4C566A";
+          blue = "0x81A1C1";
+          cyan = "0x8FBCBB";
+          green = "0xA3BE8C";
+          magenta = "0xB48EAD";
+          red = "0xBF616A";
+          white = "0xECEFF4";
+          yellow = "0xEBCB8B";
+        };
 
-        normal.black = "0x3B4252";
-        normal.blue = "0x81A1C1";
-        normal.cyan = "0x88C0D0";
-        normal.green = "0xA3BE8C";
-        normal.magenta = "0xB48EAD";
-        normal.red = "0xBF616A";
-        normal.white = "0xE5E9F0";
-        normal.yellow = "0xEBCB8B";
+        normal = {
+          black = "0x3B4252";
+          blue = "0x81A1C1";
+          cyan = "0x88C0D0";
+          green = "0xA3BE8C";
+          magenta = "0xB48EAD";
+          red = "0xBF616A";
+          white = "0xE5E9F0";
+          yellow = "0xEBCB8B";
+        };
 
-        primary.background = "0x2E3440";
-        primary.foreground = "0xD8DEE9";
+        primary = {
+          background = "0x2E3440";
+          foreground = "0xD8DEE9";
+        };
       };
 
       bell = {
