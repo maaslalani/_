beed3b489de5c5d5b14a29a36a0c0ee9e377c931
Maas Lalani <maas@lalani.dev>
2022-07-30 18:54:22 -0400

    wiki cd back

--- modules/shell.nix
+++ modules/shell.nix
@@ -164,7 +164,7 @@ in
 
       scratch = "FILE=`mktemp /tmp/scratch.XXXXXX`; vim $FILE +startinsert && pbcopy < $FILE; rm $FILE";
       weather = "curl http://v2.wttr.in";
-      wiki = "cd $HOME/wiki && vim index.norg";
+      wiki = "cd $HOME/wiki && vim index.norg && cd -";
 
       x = "exit";
       demo =
