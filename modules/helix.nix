e2b82ec7da0ff1f422a08747ef836edf3af11128
Maas Lalani <maas@lalani.dev>
2022-11-21 17:30:30 -0500

    bump flakes

--- modules/helix.nix
+++ modules/helix.nix
@@ -61,23 +61,6 @@ in {
         grammar = "scheme";
         roots = [];
       }
-      {
-        name = "json";
-        file-types = ["lock" "json"];
-        language-server = {command = "vscode-json-languageserver";};
-      }
-      {
-        name = "html";
-        language-server = {command = "html-languageserver";};
-      }
-      {
-        name = "css";
-        language-server = {command = "css-languageserver";};
-      }
-      {
-        name = "javascript";
-        auto-format = true;
-      }
     ];
     themes = {
       charm = let
