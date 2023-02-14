3fcebc42e549b869eb35346b7e0dbbca5c054a31
Maas Lalani <maas@lalani.dev>
2023-02-14 11:26:19 -0800

    update flakes

--- modules/helix.nix
+++ modules/helix.nix
@@ -14,7 +14,6 @@ in {
         gutters = ["diff" "line-numbers" "spacer" "diagnostics"];
         cursorline = true;
         cursor-shape.insert = "bar";
-        file-picker.max-depth = 3;
         true-color = true;
         lsp.display-messages = true;
       };
