036887098fd7d72a14edcc54d289b4943e1e571c
Maas Lalani <maas@lalani.dev>
2023-10-29 10:54:50 -0400

    feat: remove aliases and use $EDITOR

--- modules/gh.nix
+++ modules/gh.nix
@@ -5,10 +5,7 @@
       aliases = {
         clone = "repo clone";
         co = "pr checkout";
-        open = "repo view --web";
-        pr = "pr create";
       };
-      editor = "nvim";
     };
   };
 }
