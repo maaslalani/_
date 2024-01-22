3fe51008ae29113ae16beb0e06e270a3c055e944
Maas Lalani <maas@lalani.dev>
2024-01-22 09:23:30 -0500

    notion calendar

--- modules/skhd.nix
+++ modules/skhd.nix
@@ -2,13 +2,13 @@
   skhdPath = "${config.xdg.configHome}/skhd";
 in {
   home.file."${skhdPath}/skhdrc".text = ''
-    meh - b : open -a Safari
-    meh - c : open -a Calendar
-    meh - d : open -a Discord
-    meh - f : open -a Finder
-    meh - n : open -a Obsidian
-    meh - r : open -a Reminders
-    meh - t : open -a Ghostty
+    meh - b : open -a "Safari"
+    meh - c : open -a "Notion Calendar"
+    meh - d : open -a "Discord"
+    meh - f : open -a "Finder"
+    meh - n : open -a "Obsidian"
+    meh - r : open -a "Reminders"
+    meh - t : open -a "Ghostty"
 
     meh - p : open /System/Library/PreferencePanes/Passwords.prefPane
 
