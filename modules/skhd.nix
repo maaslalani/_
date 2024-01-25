0897edbc5cf1694596a3bab21fb198f99458e0a8
Maas Lalani <maas@lalani.dev>
2024-01-25 13:25:42 -0500

    move left and right

--- modules/skhd.nix
+++ modules/skhd.nix
@@ -14,7 +14,10 @@ in {
 
     ctrl - t : skhd --key escape
 
-    meh - h : yabai -m window --display 1 && yabai -m display --focus 1 && yabai -m window --grid 1:1:1:1:1:1
-    meh - l : yabai -m window --display 2 && yabai -m display --focus 2 && yabai -m window --grid 1:1:1:1:1:1
+    meh - h : yabai -m window --grid 2:2:0:0:1:2
+    meh - l : yabai -m window --grid 2:2:1:0:1:2
+
+    meh - H : yabai -m window --display 1 && yabai -m display --focus 1 && yabai -m window --grid 1:1:1:1:1:1
+    meh - L : yabai -m window --display 2 && yabai -m display --focus 2 && yabai -m window --grid 1:1:1:1:1:1
   '';
 }
