7ebe5c77e83fea253cf2d0efaa8b0adeebc03926
Maas Lalani <maas@lalani.dev>
2024-01-30 10:42:42 -0500

    next / prev display

--- modules/skhd.nix
+++ modules/skhd.nix
@@ -14,12 +14,10 @@ in {
 
     ctrl - t : skhd --key escape
 
-    alt - l : yabai -m window --focus east
-    alt - h : yabai -m window --focus west
+    alt - l : yabai -m window --focus east || yabai -m display --focus next
+    alt - h : yabai -m window --focus west || yabai -m display --focus prev
     shift + alt - l : yabai -m window --grid 1:2:1:1:1:1
     shift + alt - h : yabai -m window --grid 1:2:0:1:1:1
-    cmd + ctrl - l : yabai -m display --focus next
-    cmd + ctrl - h : yabai -m display --focus prev
 
     meh - l : yabai -m window --display next; yabai -m display --focus next
     meh - h : yabai -m window --display prev; yabai -m display --focus prev
